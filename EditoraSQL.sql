62a622dfcb88fb0044a5fa66a20e40d5aef6f49e
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-10-01 19:56:45 -0300

    Update EditoraSQL.sql

--- EditoraSQL.sql
+++ EditoraSQL.sql
@@ -3,25 +3,25 @@
  drop database livraria;
  
  create table editora(
- cod_editora int auto_increment primary key,
- descricao varchar(45) not null,
- endereco varchar(45) not null
+  cod_editora int auto_increment primary key,
+  descricao varchar(45) not null,
+  endereco varchar(45) not null
  );
  
 alter table editora change column descricao nome varchar(45) not null;
 ALTER TABLE editora  ADD COLUMN cod_grupo INT, ADD CONSTRAINT fk_grupo FOREIGN KEY (cod_grupo)  REFERENCES grupo(id_grupo);
 
  
- create table livro(
-cod_livro int auto_increment primary key,
-isbn varchar(45) not null,
-titulo varchar(45) not null,
-autor varchar(45) not null,
-num_edicao int,
-preco float not null,
-editora_cod_editora int not null,
-foreign key (editora_cod_editora) references editora(cod_editora)
- );
+create table livro(
+ cod_livro int auto_increment primary key,
+ isbn varchar(45) not null,
+ titulo varchar(45) not null,
+ autor varchar(45) not null,
+ num_edicao int,
+ preco float not null,
+ editora_cod_editora int not null,
+ foreign key (editora_cod_editora) references editora(cod_editora)
+);
  alter table livro change column isbn isbn varchar(45) unique not null;
  alter table livro modify column preco float default 10;
  alter table livro drop num_edicao;
@@ -29,27 +29,24 @@ foreign key (editora_cod_editora) references editora(cod_editora)
  
  
  
- create table autor(
-cod_autor int auto_increment primary key,
-nome varchar(45) not null,
-sexo char(1),
-data_nascimento date
+create table autor(
+ cod_autor int auto_increment primary key,
+ nome varchar(45) not null,
+ sexo char(1),
+ data_nascimento date
 );
 alter table autor change column sexo sexo varchar(1);
 
 create table livro_autor(
-cod_livro_fk int,
-cod_autor_fk int,
-foreign key (cod_livro_fk) references livro(cod_livro),
-foreign key (cod_autor_fk) references autor(cod_autor)
-
+ cod_livro_fk int,
+ cod_autor_fk int,
+ foreign key (cod_livro_fk) references livro(cod_livro),
+ foreign key (cod_autor_fk) references autor(cod_autor)
 );
 
 create table grupo(
-id_grupo int primary key auto_increment,
-nome varchar(45)
-
-
+ id_grupo int primary key auto_increment,
+ nome varchar(45)
 );
 
 
