bfde4f50b4177a9ac7158956a01ed778493b4495
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-10-16 22:03:37 -0300

    Update BancoSQL-Parte1.sql

--- BancoSQL-Parte1.sql
+++ BancoSQL-Parte1.sql
@@ -18,6 +18,7 @@ create table agencia(
 );
 
 
+
 create table conta(
 	num_conta varchar(7) primary key not null,
     saldo float not null,
@@ -25,6 +26,7 @@ create table conta(
     num_agencia int not null,
     foreign key (num_agencia) references agencia(numero_agencia)
 );
+
 
 create table historico(
 	cpf varchar(14) not null,
@@ -41,6 +43,8 @@ create table cliente(
     endereco varchar(100),
     sexo char(1)
 );
+alter table cliente add email varchar(60);
+select cpf, endereco from cliente where nome like 'c%';
 
 create table telefone_cliente(
 	cpf_cliente varchar(45) not null,
@@ -49,6 +53,44 @@ create table telefone_cliente(
     foreign key (cpf_cliente) references cliente(cpf)
 
 );
+
+
+
+
+
+
+insert into banco(nome) values ('Banco do Brasil');
+insert into banco(codigo,nome) values (4,'CEF');
+insert into banco(nome) values ('Inter');
+
+
+
+insert into agencia(numero_agencia,endereco,cod_banco) values ('0562','Rua Joaquim Teixeira Alves, 1555','4');
+insert into agencia(numero_agencia,endereco,cod_banco) values ('3153','Av. Marcelino Pires, 1960','1');
+
+
+
+insert into cliente(cpf,nome,sexo,endereco) values ('111.222.333-44','Jennifer B Souza','F','Rua Cuiabá, 1050');
+insert into cliente(cpf,nome,sexo,endereco) values ('666.777.888-99','Caetano K Lima','M','Rua Ivinhema, 879');
+insert into cliente(cpf,nome,sexo,endereco) values ('555.444.777-33','Silvia Macedo','F','Rua Estados Unidos, 735');
+
+
+
+insert into conta(num_conta, saldo, tipo_conta, num_agencia) values ('86340-2', 763.05, 2 , 3153);
+insert into conta(num_conta, saldo, tipo_conta, num_agencia) values ('23584-7', 3879.12, 1 , 0562);
+
+
+
+insert into historico(cpf, num_conta, data_inicio) values ('111.222.333-44','23584-7','1997-12-17');
+insert into historico(cpf, num_conta, data_inicio) values ('666.777.888-99','23584-7','1997-12-17');
+insert into historico(cpf, num_conta, data_inicio) values ('555.444.777-33','86340-2','2010-11-29');
+
+
+
+insert into telefone_cliente(cpf_cliente,telefone) values('111.222.333-44','(67)3422-7788');
+insert into telefone_cliente(cpf_cliente,telefone) values('666.777.888-99','(67)3423-9900');
+insert into telefone_cliente(cpf_cliente,telefone) values('666.777.888-99','(67)8121-8833');
+
 
 
 
