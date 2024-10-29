ff36a34c02bbbfeec491150f666d51960653f6e9
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2024-10-29 00:03:53 -0300

    Update CREATE DATABASE universidade1.sql

--- CREATE DATABASE universidade1.sql
+++ CREATE DATABASE universidade1.sql
@@ -53,21 +53,14 @@ VALUES
 ('João da Silva', '123.456.789-00', '1990-01-01', 'M'),
 ('Maria da Costa', '987.654.321-00', '1991-02-02', 'F'),
 ('Pedro dos Santos', '098.765.432-10', '1992-03-03', 'M');
-/*1*/
-select nome, cpf, data_nascimento from aluno;
-/*2*/
-select nome, cpf, data_nascimento from aluno where data_nascimento > 1990-01-01;
-/*5*/
-select id_aluno, nota from matricula  join matricula_disciplina on matricula.id_matricula=matricula_disciplina.id_matricula 
-join disciplina on disciplina.id_disciplina=matricula_disciplina.id_disciplina 
-where nome = 'Programação Orientada a Objetos' order by nota asc limit 1;
+
 
 INSERT INTO curso (nome, carga_horaria)
 VALUES
 ('Ciência da Computação', 2400),
 ('Engenharia de Software', 2800),
 ('Administração', 2200);
-select nome from curso order by carga_horaria desc limit 1;
+
 
 INSERT INTO matricula (id_aluno, id_curso, data_inicio, data_fim)
 VALUES
@@ -81,13 +74,7 @@ VALUES
 ('Algoritmos e Estruturas de Dados', 60),
 ('Programação Orientada a Objetos', 60),
 ('Bancos de Dados', 60);
-/*4*/
-alter table matricula_disciplina Drop foreign key matricula_disciplina_ibfk_2;
-	alter table matricula_disciplina
-		add constraint id_disciplina foreign key (id_disciplina)
-        references disciplina(id_disciplina) on delete cascade;
-        
-delete from disciplina where carga_horaria <20;
+
 
 INSERT INTO matricula_disciplina (id_matricula, id_disciplina, nota)
 VALUES
@@ -102,3 +89,43 @@ VALUES
 (3, 3, 10);
 
 
+/*1*/
+select nome,cpf,data_nascimento from aluno;
+
+/*2*/
+select nome,cpf,sexo from aluno where data_nascimento > 1990;
+
+/*3*/
+select nome from curso order by carga_horaria desc limit 1;
+
+/*4*/
+INSERT INTO disciplina (nome, carga_horaria) values ('Poo', 15);
+select * from disciplina;
+
+ALTER TABLE matricula_disciplina DROP FOREIGN KEY matricula_disciplina_ibfk_2;
+      ALTER TABLE matricula_disciplina
+	     ADD CONSTRAINT matricula_disciplina_ibfk_2 FOREIGN KEY (id_disciplina) 
+         REFERENCES disciplina(id_disciplina) ON DELETE CASCADE;
+         
+         delete from disciplina where carga_horaria < 20;
+         
+/*5*/
+select id_aluno from matricula
+join matricula_disciplina ON matricula.id_matricula=matricula_disciplina.id_matricula
+join disciplina ON disciplina.id_disciplina=matricula_disciplina.id_disciplina
+where nome = 'Programação Orientada a Objetos' order by nota asc limit 1;
+
+/*6*/
+select id_aluno from matricula
+join curso ON matricula.id_curso=curso.id_curso
+where carga_horaria > 2400;
+
+/*7*/
+select aluno.nome, curso.nome , carga_horaria from aluno
+join matricula ON aluno.id_aluno = matricula.id_aluno
+join curso ON matricula.id_curso=curso.id_curso;
+
+/*8*/
+update curso
+   set carga_horaria = 300
+   where nome = "Administração";
